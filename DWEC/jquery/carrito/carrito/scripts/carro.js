1cb3548e5da154f2a70d8cc5e7b93e5fa0dae3c9
Luis Marco <luismarco.web@gmail.com>
2018-12-03 19:09:56 +0100

    carrito

---
+++ DWEC/jquery/carrito/carrito/scripts/carro.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.item').css('background', '#cecece');
+    $('#cart_items').css('border','4px solid black');
+    $('img').css('border', '1px solid blue');
+    $('.item').children('label').css('text-decoration', 'underline');
+    $('#cart_container button').css('');
+
+});
