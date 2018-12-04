182ad1fa9e2f56c687cafc65170dd8f7a0a64d90
luis <luismarco.web@gmail.com>
2018-12-04 16:15:27 +0100

    sdsd

--- DWEC/jquery/carrito/carrito/scripts/carro.js
+++ DWEC/jquery/carrito/carrito/scripts/carro.js
@@ -1,9 +1,15 @@
 $(function(){
-
     $('.item').css('background', '#cecece');
     $('#cart_items').css('border','4px solid black');
     $('img').css('border', '1px solid blue');
     $('.item').children('label').css('text-decoration', 'underline');
-    $('#cart_container button').css('');
-
+    $('#cart_container button').css('color', 'red');
+    $('.item label').siblings('label').css('color','white');
+    $(":contains('€'),input").css('color','green');
+    $("div:empty").css('background-color','yellow');
+    $(".item").children().last().css('background-color','red');
+    $(".item:first-child").css('background-color','red');
+    $('item img').each(function(indice, elemento){
+        console.log(indice+ " " +elemento);
+    });
 });
