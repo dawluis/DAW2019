9a9e5b1e1edbeb450daf2d5404452cc6bea760fe
Luis Marco <luismarco.web@gmail.com>
2018-12-10 20:45:03 +0100

    dfg

--- DWEC/jquery/carrito/scripts/carro2.js
+++ DWEC/jquery/carrito/scripts/carro2.js
@@ -1,16 +1,33 @@
 $(function(){
+    var sumaPrecio=0;
+    
     $('.item').click(function(e){
-       var x=e.target.parentNode.id;
+         /*****************STOCK**************************************** */
+        var x=e.target.parentNode.id;
         var stock=$('#'+x).children("label:contains('Stock')").text();
         var partes= stock.split(" ");
-        var Iprecio=parseInt(partes[1]);
+        var iStock=parseInt(partes[1]);
         
-        if(Iprecio<=0){
-            $('#'+x).children("label:contains('Stock')").text("Stock "+Iprecio).css("color","red");
+        /********************PRECIO************************************* */
+        var precio=$('#'+x).children("label:contains('€')").text();
+        var sPrecio=parseInt(precio.substring(0,2));
+        /**********************ARTICULOS COMPRADOS*********************************** */
+        var ArticulosComprados=parseInt($('#citem').val());
+        
+        if(iStock<=0){
+            $('#'+x).children("label:contains('Stock')").text("Stock "+iStock).addClass('agotado');
         }else{
-            Iprecio--;
-            $('#'+x).children("label:contains('Stock')").text("Stock "+Iprecio);
-
+            iStock--;
+            ArticulosComprados++;
+            sumaPrecio=sumaPrecio+sPrecio;
+            console.log(sumaPrecio);
+           
+            $('#citem').val(ArticulosComprados);
+    
+            $('#citem').text(ArticulosComprados);
+            $('#cprice').attr("value",sumaPrecio+" €");
+            $('#'+x).children("label:contains('Stock')").text("Stock "+iStock);
+            $('#'+x).children("label:contains('€')").val(sumaPrecio);
         }
 
 
