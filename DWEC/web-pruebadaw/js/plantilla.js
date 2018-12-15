057e47721b2a5d6d4966444f90e618a9336121cd
luis <luismarco.web@gmail.com>
2018-12-15 17:17:58 +0100

    sumando efectos

--- DWEC/web-pruebadaw/js/plantilla.js
+++ DWEC/web-pruebadaw/js/plantilla.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-
+  var x= $('#efecto').position();
+  $('#efecto').hide();
+  $(document).scroll(function(){
+    var distancia=($(document).scrollTop());
+    if(distancia > x.top/2){
+        //$('#efecto').fadeIn('slow');
+        $('#efecto').animate({left: '250px'});
+    }else if($(document).scrollTop() < x.top){
+        $('#efecto').fadeOut('slow');
+    }
+  });
+ 
 
 });
