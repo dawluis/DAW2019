394f45deb5e93ec0fa2da1d7982d84ec5650cb34
Luis Marco <luismarco.web@gmail.com>
2018-12-18 20:15:33 +0100

    asda

--- DWEC/jquery/10-TecladoyRaton/2-teclado/teclado.js
+++ DWEC/jquery/10-TecladoyRaton/2-teclado/teclado.js
@@ -1,17 +1,11 @@
-var principal= document.getElementById("principal");
-
-
-
-
 function muestraInformacion(event){
-  var typo=event.type;
   var e= event || window.event;
   var codigo=e.keyCode;
   var propiedad=e.charCode;
   var caracterPulsado= String.fromCharCode(propiedad);
-  principal.innerHTML= 
-  "Codigo: [" + e.keyCode + "]<br>"+
-  "Carácter pulsado: ["+ String.fromCharCode(e.keyCode)+"]";
+  teclado.innerHTML= 
+  "<h2>Teclado</h2><p>Codigo: [" + e.keyCode + "]</p>"+
+  "<p>Carácter pulsado: ["+ String.fromCharCode(e.keyCode)+"]</p>";
 
 }
 document.onkeypress = muestraInformacion;
